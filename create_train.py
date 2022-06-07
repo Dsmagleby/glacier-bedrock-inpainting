ff235f8e07649933f6b15782f11857c141ef0ab7
Daniel Sørensen <dsmagleby@gmail.com>
2022-06-07 11:39:24 +0200

    creat_train.py implemented, not tested

--- create_train.py
+++ create_train.py
@@ -11,8 +11,8 @@ import numpy as np
 from oggm import utils
 from tqdm import tqdm
 
-from ..libs.utils import coords_to_xy, contains_glacier
-from ..libs.set_generation import create_test_images_from_glacier_center, flow_train_dataset
+from Pconv.libs.utils import coords_to_xy, contains_glacier
+from Pconv.libs.set_generation import create_test_images_from_glacier_center, flow_train_dataset
 
 
 
@@ -20,11 +20,13 @@ parser = argparse.ArgumentParser(description='Create DEM mosaic from DEM tiles')
 parser.add_argument("--input",  type=str, default=None, help="path to the DEM tile folder")
 parser.add_argument("--outdir", type=str, default="DEM_files/mosaic/mosaic.tif", help="path for the output file")
 
+# currently not used
 parser.add_argument("--region",  type=int, default=11, help="RGI region")
 parser.add_argument("--shape",  type=int, default=256, help="Size of test patches")
 parser.add_argument("--version",  type=str, default='62', help="RGI version")
 parser.add_argument("--epsg",  type=str, default="EPSG:4326", help="DEM projection")
 
+# adjust naming and sampling behavior 
 parser.add_argument("--postfix",  type=str, default='a', help="postfix added behind samples")
 parser.add_argument("--max_height",  type=int, default=4800, help="Max desired height of training samples")
 parser.add_argument("--average_height",  type=int, default=2187, help="Sampling average 128x128 center box condition")
@@ -34,25 +36,35 @@ parser.add_argument("--samples",  type=int, default=25000, help="Number of sampl
 def main():
 
     args = parser.parse_args()
+    
+    train_path = args.outdir + 'train/'
+    val_path = args.outdir + 'val/'
 
-    if os.path.isdir(args.outdir):
-        None
+    if os.path.isdir(train_path):
+        if os.path.isdir(train_path + 'images/'):
+            None
+        else:
+            os.mkdir(train_path + 'images/')
     else:
-        os.mkdir(args.outdir)
+        os.mkdir(train_path)
+        os.mkdir(train_path + 'images/')
 
-    # fetch RGI
-    utils.get_rgi_dir(version=args.version)
-    eu = utils.get_rgi_region_file(args.region, version=args.version)
-    gdf = gpd.read_file(eu)
+    if os.path.isdir(val_path):
+        if os.path.isdir(val_path + 'images/'):
+            None
+        else:
+            os.mkdir(val_path + 'images/')
+    else:
+        os.mkdir(val_path)
+        os.mkdir(val_path + 'images/')
 
     # load DEM
     dem = rio.open(args.input, 'r').read(1)
-    empty = np.zeros_like(dem)
+    empty = rio.open(args.input.replace('.tif', '_mask.tif'), 'r').read(1)
 
     print("Attempting to create {} samples".format(args.samples))
-    train_path = args.outdir + 'train/images/'
-    val_path = args.outdir + 'val/images/'
-    _ = flow_train_dataset(dem, empty, (args.shape, args.shape), train_path, val_path, args.max_height, 
+
+    _ = flow_train_dataset(dem, empty, (args.shape, args.shape), train_path + 'images/', val_path + 'images/', args.max_height, 
                            args.average_height, 'average', args.samples, args.postfix)
 
 
